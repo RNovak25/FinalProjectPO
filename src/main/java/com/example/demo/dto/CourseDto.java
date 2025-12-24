f5b8d7223a4522de19c259ae5282d939d072a669
RNovak25 <ruslan.novak@narxoz.kz>
2025-12-24 20:01:45 +0500

    Added DTOs for Student, Course, Teacher

---
+++ src/main/java/com/example/demo/dto/CourseDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseDto {
+    private Long id;
+    private String courseName;
+    private String description;
+    private int durationMonths;
+}
---
+++ src/main/java/com/example/demo/dto/StudentDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+}
